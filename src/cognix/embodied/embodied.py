f460ac04b183aca6f9b1bd2301edbbe846f5b3c2
Mohammad Rahmani <mohammad.rahmani.xyz@gmail.com>
2025-11-02 00:02:28 +0100

    embodied cognition added

---
+++ src/cognix/embodied/embodied.py
@@ -0,0 +1,4 @@
+class Embodied:
+    """
+    Embodied cognition represents a diverse group of theories which investigate how cognition is shaped by the bodily state and capacities of the organism.
+    """
